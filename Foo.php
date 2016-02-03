3af209d11653efc68068b4b00a1a98aaf1afee10
vagrant <Eugen Kandakov>
2016-02-03 09:36:39 +0000

    first commit

---
+++ Foo.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Backend\Util;
+
+
+use Faker\Factory;
+use Faker\ORM\Propel\EntityPopulator;
+
+class Foo {
+
+    public function Bar() {
+
+
+        $oGenerator = Factory::create();
+
+        $aAccount = array(
+            'Id' => function() use ($oGenerator) { return $oGenerator->numberBetween(1, 9); },
+            'RegionId' => function() use ($oGenerator) { return $oGenerator->numberBetween(1, 9); }
+        );
+
+        $entity = new EntityPopulator('Account');
+        $entity->mergeColumnFormattersWith($aAccount);
+        foreach ($entity->getColumnFormatters() as $column => $format) {
+            if (null !== $format) {
+                //do further stuff
+            }
+        }
+    }
+
+}
